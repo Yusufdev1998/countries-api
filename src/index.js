9be2b3daf30ef35e99bc9daf86f72cdc5a766e2a
Yusufdev1998 <114648299+Yusufdev1998@users.noreply.github.com>
2023-08-29 13:50:32 +0500

    html added

--- src/index.js
+++ src/index.js
@@ -15,6 +15,133 @@ try {
 const app = express();
 
 // app.get("/dump-data", dumpData);
+app.get("/", (req, res)=> {
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Countries API</title>
+    <style>
+      @import url(https://fonts.googleapis.com/css2?family=Open+Sans:wght@100;200;300;400;500;600;800&display=swap);
+
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+
+      body {
+        font-family: "Open Sans", sans-serif;
+        background: #f0f0f0;
+      }
+
+      .container {
+        width: 100%;
+        max-width: 1200px;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: 40px;
+        padding-right: 40px;
+      }
+
+      .list {
+        margin-top: 15px;
+        margin-bottom: 15px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
+        list-style: none;
+      }
+      .item {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        padding: 10px 20px 20px;
+        background: #fff;
+        border-radius: 10px;
+      }
+      .item-title {
+        font-size: 28px;
+      }
+      .item-desc {
+        font-size: 18px;
+      }
+      .link-wrapper {
+        padding: 10px;
+        background: #929292;
+        border-radius: 10px;
+      }
+      .item-link {
+        text-decoration: none;
+        color: #fff;
+      }
+      .item-link:hover {
+        color: rgb(102, 0, 255);
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <ul class="list">
+        <li class="item">
+          <h2 class="item-title">All</h2>
+          <p class="item-desc">Ma'lumotni to'liq olish</p>
+
+          <div class="link-wrapper">
+            <a
+              class="item-link"
+              href="https://countries-api-v7sn.onrender.com/countries?limit=250"
+              target="_blank"
+              >https://countries-api-v7sn.onrender.com/countries?limit=250</a
+            >
+          </div>
+        </li>
+        <li class="item">
+          <h2 class="item-title">By Name(Slug)</h2>
+          <p class="item-desc">Bitta davlatni ma'lumotini olish</p>
+
+          <div class="link-wrapper">
+            <a
+              class="item-link"
+              href="https://countries-api-v7sn.onrender.com/countries/slug/uzbekistan"
+              target="_blank"
+              >https://countries-api-v7sn.onrender.com/countries/slug/${country-name}</a
+            >
+          </div>
+        </li>
+        <li class="item">
+          <h2 class="item-title">Search</h2>
+          <p class="item-desc">Davlatni nomi bo'yicha</p>
+
+          <div class="link-wrapper">
+            <a
+              class="item-link"
+              href="https://countries-api-v7sn.onrender.com/countries?search=uzbek"
+              target="_blank"
+              >https://countries-api-v7sn.onrender.com/countries?search=${text}</a
+            >
+          </div>
+        </li>
+        <li class="item">
+          <h2 class="item-title">By Region</h2>
+          <p class="item-desc">Region bo'yicha ma'lumotni olish</p>
+
+          <div class="link-wrapper">
+            <a
+              class="item-link"
+              href="https://countries-api-v7sn.onrender.com/countries?region=Africa"
+              target="_blank"
+              >https://countries-api-v7sn.onrender.com/countries?region=${Region-name}</a
+            >
+          </div>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>`)
+})
 
 app.get("/countries", getCountries);
 app.get("/countries/slug/:slug", getCountryBySlug);
